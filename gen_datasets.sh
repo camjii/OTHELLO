d41b48a51a5c7b1f007bef1d6d26a918a69b97ac
Cameron Kruger <cameronkruger@Camerons-MacBook-Air.local>
2026-01-18 19:40:47 -0800

    downloaded qwen

--- gen_datasets.sh
+++ gen_datasets.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-mkdir datasets;
+mkdir datasets translations models;
 curl https://raw.githubusercontent.com/KGQA/QALD_9_plus/refs/heads/main/data/qald_9_plus_test_wikidata.json > datasets/qald.json;
 
 curl http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_train_v1.1.json > datasets/hotpot.json;
 
 curl https://raw.githubusercontent.com/amazon-science/mintaka/refs/heads/main/data/mintaka_train.json > datasets/mintaka.json;
 
-mkdir translations;
